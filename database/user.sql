6556fd549a4ab24966020db31a5e1a1a8dbabe5e
bitacademy-poscodx <poscoict.bitacademy@gmail.com>
2024-05-27 11:48:02 +0900

    refactoring #1

--- database/user.sql
+++ database/user.sql
@@ -1,4 +1,7 @@
 desc user;
 
 -- join
-insert into user values(null, '관리자', 'admin@mysicte.com', password('1234'), 'male', surrent_date());
+insert into user values(null, '관리자', 'admin@mysicte.com', password('1234'), 'male', current_date());
+
+-- test
+select * from user; 
