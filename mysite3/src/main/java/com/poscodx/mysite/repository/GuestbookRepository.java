5a2ec4cc3a11c79e2aec49a0ffc8ec1c840cd3aa
bitacademy-poscodx <poscoict.bitacademy@gmail.com>
2024-06-10 15:31:11 +0900

    security: Login&logout Interceptor

--- mysite3/src/main/java/com/poscodx/mysite/repository/GuestbookRepository.java
+++ mysite3/src/main/java/com/poscodx/mysite/repository/GuestbookRepository.java
@@ -5,7 +5,6 @@ import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
-import org.springframework.util.StopWatch;
 
 import com.poscodx.mysite.vo.GuestbookVo;
 
