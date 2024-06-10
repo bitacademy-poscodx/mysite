93441e008a1660b18e97aec51269bb68e056dcc6
bitacademy-poscodx <poscoict.bitacademy@gmail.com>
2024-06-10 15:56:07 +0900

    security - AuthInterceptor#1

--- mysite3/src/main/java/com/poscodx/mysite/controller/BoardController.java
+++ mysite3/src/main/java/com/poscodx/mysite/controller/BoardController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.poscodx.mysite.security.Auth;
 import com.poscodx.mysite.service.BoardService;
 import com.poscodx.mysite.vo.BoardVo;
 import com.poscodx.mysite.vo.UserVo;
@@ -97,14 +98,9 @@ public class BoardController {
 				"&kwd=" + WebUtil.encodeURL( keyword, "UTF-8" );
 	}
 
+	@Auth
 	@RequestMapping(value="/write", method=RequestMethod.GET)	
-	public String write(HttpSession session) {
-		// access control
-		UserVo authUser = (UserVo)session.getAttribute("authUser");
-		if(authUser == null) {
-			return "redirect:/";
-		}
-		////////////////////////
+	public String write() {
 		return "board/write";
 	}
 
