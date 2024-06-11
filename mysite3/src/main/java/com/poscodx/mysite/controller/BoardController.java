f778ba3fb118050b3dc0f5e5a9bcd7dd1d0dd13c
bitacademy-poscodx <poscoict.bitacademy@gmail.com>
2024-06-11 09:55:50 +0900

    security - argumentResolver + @AuthUser

--- mysite3/src/main/java/com/poscodx/mysite/controller/BoardController.java
+++ mysite3/src/main/java/com/poscodx/mysite/controller/BoardController.java
@@ -1,9 +1,6 @@
 package com.poscodx.mysite.controller;
 
 import java.util.Map;
-
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.poscodx.mysite.security.Auth;
+import com.poscodx.mysite.security.AuthUser;
 import com.poscodx.mysite.service.BoardService;
 import com.poscodx.mysite.vo.BoardVo;
 import com.poscodx.mysite.vo.UserVo;
@@ -50,29 +48,17 @@ public class BoardController {
 	@Auth
 	@RequestMapping("/delete/{no}")
 	public String delete(
-		HttpSession session,
+		@AuthUser UserVo authUser,
 		@PathVariable("no") Long boardNo,
 		@RequestParam(value="p", required=true, defaultValue="1") Integer page,
 		@RequestParam(value="kwd", required=true, defaultValue="") String keyword) {		
-
-		UserVo authUser = (UserVo)session.getAttribute("authUser");
-		if(authUser == null) {
-			return "redirect:/";
-		}
-		
 		boardService.deleteContents(boardNo, authUser.getNo());
 		return "redirect:/board?p=" + page + "&kwd=" + WebUtil.encodeURL(keyword, "UTF-8");
 	}
 	
 	@Auth
 	@RequestMapping("/modify/{no}")	
-	public String modify(HttpSession session, @PathVariable("no") Long no, Model model) {
-
-		UserVo authUser = (UserVo)session.getAttribute("authUser");
-		if(authUser == null) {
-			return "redirect:/";
-		}
-		
+	public String modify(@AuthUser UserVo authUser, @PathVariable("no") Long no, Model model) {
 		BoardVo boardVo = boardService.getContents(no, authUser.getNo());
 		model.addAttribute("boardVo", boardVo);
 		return "board/modify";
@@ -81,16 +67,10 @@ public class BoardController {
 	@Auth
 	@RequestMapping(value="/modify", method=RequestMethod.POST)	
 	public String modify(
-		HttpSession session, 
+		@AuthUser UserVo authUser, 
 		BoardVo boardVo,
 		@RequestParam(value="p", required=true, defaultValue="1") Integer page,
 		@RequestParam(value="kwd", required=true, defaultValue="") String keyword) {		
-
-		UserVo authUser = (UserVo)session.getAttribute("authUser");
-		if(authUser == null) {
-			return "redirect:/";
-		}
-		
 		boardVo.setUserNo(authUser.getNo());
 		boardService.modifyContents(boardVo);
 		return "redirect:/board/view/" + boardVo.getNo() + 
@@ -107,20 +87,13 @@ public class BoardController {
 	@Auth
 	@RequestMapping(value="/write", method=RequestMethod.POST)	
 	public String write(
-		HttpSession session,
+		@AuthUser UserVo authUser,
 		@ModelAttribute BoardVo boardVo,
 		@RequestParam(value="p", required=true, defaultValue="1") Integer page,
 		@RequestParam(value="kwd", required=true, defaultValue="") String keyword) {
-		// access control
-		UserVo authUser = (UserVo)session.getAttribute("authUser");
-		if(authUser == null) {
-			return "redirect:/";
-		}
-		////////////////////////		
-		
+
 		boardVo.setUserNo(authUser.getNo());
 		boardService.addContents(boardVo);
-		
 		return	"redirect:/board?p=" + page + "&kwd=" + WebUtil.encodeURL(keyword, "UTF-8");
 	}
 
