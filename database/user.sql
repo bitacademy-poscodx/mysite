102000da8b8ee22a4b3fe76ddf1bb7586a4d4b97
bitacademy-poscodx <poscoict.bitacademy@gmail.com>
2024-06-24 09:57:51 +0900

    BCrypt

--- database/user.sql
+++ database/user.sql
@@ -7,8 +7,9 @@ insert into user values(null, '관리자', 'admin@mysite.com', password('1234'),
 select no, name from user where email = 'kickscar@gmail.com' and password=password('1234');
 
 -- test
+desc user;
 select * from user;
 
 -- role 추가
 alter table user add column role enum('ADMIN', 'USER') not null default 'USER'; 
-update user set role='ADMIN' where no = 1;
+update user set role='ADMIN' where no = 20;
